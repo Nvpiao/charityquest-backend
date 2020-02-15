211e61f19a5666338ae08674f0c53ec481821621
Ming Liu <mliu54@sheffield.ac.uk>
2020-02-15 19:26:30 +0000

    feat: Base code of porject
    Base code of team project.

---
+++ src/main/java/com/forever17/project/charity/charityquest/CharityquestApplication.java
@@ -0,0 +1,13 @@
+package com.forever17.project.charity.charityquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityquestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharityquestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forever17/project/charity/charityquest/CharityquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forever17.project.charity.charityquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
